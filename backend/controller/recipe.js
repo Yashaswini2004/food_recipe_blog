511a415bbb8a8bd819f017f51c92c98c69624600
Yashaswini2004 <yashaswinic31@gmail.com>
2025-08-30 23:04:14 +0530

    router and controller setup

---
+++ backend/controller/recipe.js
@@ -0,0 +1,16 @@
+const getRecipes=(req,res)=>{
+    res.json({message:"hello"})
+}
+const getRecipe=(req,res)=>{
+    res.json({message:"hello"})
+}
+const addRecipes=(req,res)=>{
+    res.json({message:"hello"})
+}
+const editRecipes=(req,res)=>{
+    res.json({message:"hello"})
+}
+const deleteRecipes=(req,res)=>{
+    res.json({message:"hello"})
+}
+module.exports={getRecipes,getRecipe,addRecipes,editRecipes,deleteRecipes};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express=require("express");
+const dotenv=require('dotenv').config()
 const app=express();
 const PORT=process.env.PORT|| 3000
+app.use("/recipe",require("./routes/recipe"))
 app.listen(PORT,(err)=>{
     console.log(`app is listening on port ${PORT}`);
 })
