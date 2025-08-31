e27c42627c8bc3a31739a7f6a59806c2fa98a415
Yashaswini2004 <yashaswinic31@gmail.com>
2025-08-31 10:55:30 +0530

    backend

--- backend/controller/recipe.js
+++ backend/controller/recipe.js
@@ -1,14 +1,34 @@
-const getRecipes=(req,res)=>{
-    res.json({message:"hello"})
+const Recipe=require("../models/recipe")
+const getRecipes=async (req,res)=>{
+   const recipes=await Recipe.find();
+   return res.json(recipes);
 }
-const getRecipe=(req,res)=>{
-    res.json({message:"hello"})
+const getRecipe=async (req,res)=>{
+   const recipe=await Recipe.findById(req.params.id);
+   return res.json(recipe);
 }
-const addRecipes=(req,res)=>{
-    res.json({message:"hello"})
+const addRecipes=async (req,res)=>{
+   const {title,ingredients,instruction,time}=req.body;
+   if(!title || !ingredients || !instruction){
+    res.json({message:"Required fields can't br empty"})
+   }
+   const newRecipe=await Recipe.create({
+    title,ingredients,instruction,time
+   })
+   return res.json(newRecipe);
 }
-const editRecipes=(req,res)=>{
-    res.json({message:"hello"})
+const editRecipes=async (req,res)=>{
+    const {title,ingredients,instruction,time}=req.body;
+    let recipe=await Recipe.findById(req.params.id);
+    try{
+    if(recipe){
+        await Recipe.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        return res.json({title,ingredients,instruction,time})
+    }
+}
+catch(err){
+   return res.status(404).json({message:"error"})
+}
 }
 const deleteRecipes=(req,res)=>{
     res.json({message:"hello"})
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express=require("express");
+const connectDb=require("./config/connectionDb");
 const dotenv=require('dotenv').config()
 const app=express();
-const PORT=process.env.PORT|| 3000
+const PORT=process.env.PORT|| 3000;
+connectDb();
+app.use(express.json())
 app.use("/recipe",require("./routes/recipe"))
 app.listen(PORT,(err)=>{
     console.log(`app is listening on port ${PORT}`);
